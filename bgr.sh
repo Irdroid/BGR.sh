8524576e269ddff1fac042089fe515920c80f363
Joro <georgi.bakalski@gmail.com>
2022-12-15 21:37:21 +0200

    Added more stations, changed the way URLs are added

--- bgr.sh
+++ bgr.sh
@@ -27,17 +27,36 @@ Green='\033[42m'       # Green
 White='\033[47m'       # White
 
 
-STATIONS_LIST='Energy FM+ Nova Quit'
+STATIONS_LIST+="Energy "
+STATIONS_LIST+="FM+ "
+STATIONS_LIST+="Nova "
+STATIONS_LIST+="Radio1 "
+STATIONS_LIST+="Radio1Rock "
+STATIONS_LIST+="BGRadio "
+STATIONS_LIST+="Thevoice "
+STATIONS_LIST+="ZRock "
+STATIONS_LIST+="Njoy "
+STATIONS_LIST+="City "
+STATIONS_LIST+="Изход "
 
-#URL_LIST='https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_ENERGYAAC_H.aac http://193.108.24.21:8000/fmplus https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_NOVAAAC_H.aac'
+# URL адреси
+URL_LIST+='https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_ENERGYAAC_H.aac ' 
+URL_LIST+='http://193.108.24.21:8000/fmplus '
+URL_LIST+='https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_NOVAAAC_H.aac '
+URL_LIST+='https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_1AAC_H.aac '
+URL_LIST+='https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_1_ROCKAAC_H.aac '
+URL_LIST+='https://playerservices.streamtheworld.com/api/livestream-redirect/BG_RADIOAAC_H.aac '
+URL_LIST+='https://bss1.neterra.tv/thevoicefm/thevoicefm.m3u8 '
+URL_LIST+='http://193.108.24.6:8000/zrock '
+URL_LIST+='http://46.10.150.243:80/njoy.mp3 '
+URL_LIST+='http://149.13.0.80/city64 '
+#read -r -d '' URL_LIST << EOM
 
-read -r -d '' URL_LIST << EOM
+#	https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_ENERGYAAC_H.aac # Energy
+#	http://193.108.24.21:8000/fmplus	# FMPLUS
+#	https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_NOVAAAC_H.aac # NOVA
 
-	https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_ENERGYAAC_H.aac # Energy
-	http://193.108.24.21:8000/fmplus	# FMPLUS
-	https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_NOVAAAC_H.aac # NOVA
-
-EOM
+#EOM
 
 
 
@@ -102,12 +121,41 @@ select station in $STATIONS_LIST
 			break;; 
 		FM+)
 			Play "FM+"
-			break;;
-
+			break
+		;;
 	 	Nova)
 	 		Play "Nova"
-	 		break;;
-	 	Quit)
+	 		break
+	 	;;
+	 	Radio1)
+	 		Play "Radio1"
+	 		break
+	 	;;
+	 	Radio1Rock)
+                        Play "Radio1Rock"
+                        break
+                ;;
+                BGRadio)
+                        Play "BGRadio"
+                        break
+                ;;
+                Thevoice)
+                        Play "Thevoice"
+                        break
+                ;;
+                ZRock)
+                        Play "ZRock"
+                        break
+                ;;
+                Njoy)
+                        Play "Njoy"
+                        break
+                ;;
+                City)
+                	Play "City"
+                	break
+                ;;
+	 	Изход)
 	 		exit;;
 	  esac	
 
@@ -119,7 +167,8 @@ select station in $STATIONS_LIST
 
 function PRINTNAME()
 {
- printf "$White \033[0m$Green \033[0m$RED \033[0m BGR - Плеър за Българско радио [bash] "
+ printf "$White \033[0m$Green \033[0m$RED \033[0m BGR - Плеър за Българско радио"
+ echo;
 }
 
 ################################################################################
@@ -159,6 +208,48 @@ then
 	  	ffplay -nodisp -hide_banner $3 &> /dev/null &
 	 	set -- $arguments
 	  	;;
+	  Radio1)
+	  	killall ffplay &> /dev/null
+                set -- $URL_LIST
+                ffplay -nodisp -hide_banner $4 &> /dev/null &
+                set -- $arguments
+		;;
+          Radio1Rock)
+                killall ffplay &> /dev/null
+                set -- $URL_LIST
+                ffplay -nodisp -hide_banner $5 &> /dev/null &
+                set -- $arguments
+                ;;
+          BGRadio)
+                killall ffplay &> /dev/null
+                set -- $URL_LIST
+                ffplay -nodisp -hide_banner $6 &> /dev/null &
+                set -- $arguments
+                ;;
+          Thevoice)
+                killall ffplay &> /dev/null
+                set -- $URL_LIST
+                ffplay -nodisp -hide_banner $7 &> /dev/null &
+                set -- $arguments
+                ;;
+           ZRock)
+                killall ffplay &> /dev/null
+                set -- $URL_LIST
+                ffplay -nodisp -hide_banner $8 &> /dev/null &
+                set -- $arguments
+                ;;
+           Njoy)
+                killall ffplay &> /dev/null
+                set -- $URL_LIST
+                ffplay -nodisp -hide_banner $9 &> /dev/null &
+                set -- $arguments
+                ;;
+           City)
+                killall ffplay &> /dev/null
+                set -- $URL_LIST
+                ffplay -nodisp -hide_banner "${10}" &> /dev/null &
+                set -- $arguments
+                ;;
 	esac  
 fi
 
@@ -167,7 +258,7 @@ CURRENT=$1
 spinner 'Playing > ' & pid=$!
 # ffprobe -hide_banner https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_ENERGYAAC_H.aac  |& grep  "StreamTitle"
 #echo -e "\e[?16;0;200c"
-echo "b - Back | q - Quit"
+echo "b - Назад | q - Изход"
 echo "" 
 	while read -n 1 command;
  		do
